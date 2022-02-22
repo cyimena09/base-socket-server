d79e9c24d1cf1867253fe10615c35790210d4437
Emile Cyimena <cyimena09@hotmail.com>
2022-02-22 13:57:41 +0100

    socket with string

--- src/be/cyimena/Main2.java
+++ src/be/cyimena/Main2.java
@@ -1,14 +1,16 @@
 package be.cyimena;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
+public class Main2 {
 
     /**
-     * Ne lis que 1 octet
+     * Lis un ensemble de chaine de charactère
      * @param args
      * @throws Exception
      */
@@ -16,17 +18,19 @@ public class Main {
         ServerSocket ss = new ServerSocket(1234);
         System.out.println("J'attends la connexion ...");
         Socket s = ss.accept(); // on accepte la connexion lorsque le client enverra une requête
+
         System.out.println("Connexion d'une machine avec l'IP : " + s.getRemoteSocketAddress());
         InputStream is = s.getInputStream();
         OutputStream os = s.getOutputStream();
+
         System.out.println("J'attends que le client envoie une donnée ...");
-        int nb = is.read(); // !! Lis uniquement un octet (donc un trop gros chiffre sera tronqué, donc la réponse sera erroné)
-        System.out.println("On a reçu un nombre : " + nb);
-        int res = nb * 5;
-        os.write(res);
-        System.out.println("Après calcul, on retourne : " + res);
-        s.close();
+        InputStreamReader isr = new InputStreamReader(is); // permet de lire 32 bits (un caractère)
+        BufferedReader br = new BufferedReader(isr); // permet de lire une chaine de caractère
+        String data = br.readLine();
+        System.out.println("On a reçu une chaine de caractère : " + data);
+
         System.out.println("On ferme la connexion.");
+        s.close();
     }
 
 }
